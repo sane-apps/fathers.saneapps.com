2503f411a23b6abf2360729e91eb98992c3c3a4b
SJ <hi@saneapps.com>
2026-09-13 09:33:39 -0400

    Include theophanes-* tip-fragment books in site loader.
    Needed for Theophanes Confessor Chronographia Rank-1 SERIES ship.

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -106,6 +106,7 @@ OTHER_RANK1_TIP_BOOKS = sorted(
     | set(BOOKS.glob("maximus-*"))
     | set(BOOKS.glob("nicephorus-*"))
     | set(BOOKS.glob("symeon-magister-*"))
+    | set(BOOKS.glob("theophanes-*"))
 )
 TIP_FRAGMENT_BOOKS = (
     ORIGEN_PAULINE_FRAGMENT_BOOKS
